0cf682c1d1c9dd387dd5f09a0507870807c0bafa
DESKTOP-2QJH1HL\19 <ko.nagai.0801@gmail.com>
2023-12-25 12:54:40 +0900

    フォームの設置

--- js/app.js
+++ js/app.js
@@ -184,3 +184,35 @@ $(document).ready(function () {
 
   animateCharacter();
 });
+
+// 申し込みフォームの複数表示
+function toggleDisplayByDate(className, startDate, endDate) {
+  let currentDate = new Date();
+  let elements = document.getElementsByClassName(className);
+  let displayStyle =
+    currentDate >= new Date(startDate) && currentDate < new Date(endDate)
+      ? "block"
+      : "none";
+
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.display = displayStyle;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  toggleDisplayByDate(
+    "schedule1",
+    "2023-11-17T00:00:00",
+    "2024-01-22T23:59:59"
+  );
+  toggleDisplayByDate(
+    "schedule2",
+    "2023-11-30T00:00:00",
+    "2024-02-26T23:59:59"
+  );
+  toggleDisplayByDate(
+    "schedule3",
+    "2023-＊＊-＊＊T00:00:00",
+    "2024-＊＊-＊＊T23:59:59"
+  );
+});
