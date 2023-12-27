1b3eff0490487903389cbf3d9d8b4d002be404cd
DESKTOP-2QJH1HL\19 <ko.nagai.0801@gmail.com>
2023-12-27 10:45:07 +0900

    世話人紹介更新

--- js/app.js
+++ js/app.js
@@ -189,22 +189,22 @@ $(document).ready(function () {
 // imageData、bios、およびrepresentativesのデータ構造を定義します
 const imageData = [
   {
-    src: "../img/sasagawa2.webp",
+    src: "./img/sasagawa3.webp",
     repName: "代表世話人",
     repTitle: "笹川 政吉",
   },
   {
-    src: "../img/sasagawa3.webp",
+    src: "./img/sasagawa2.webp",
     repName: "代表世話人",
     repTitle: "笹川 政吉",
   },
   {
-    src: "../img/nishikawa1.webp",
-    repName: "世話人",
+    src: "./img/nishikawa1.webp",
+    repName: "副代表世話人",
     repTitle: "西川 毅",
   },
   {
-    src: "../img/tokuda1.webp",
+    src: "./img/tokuda1.webp",
     repName: "世話人",
     repTitle: "徳田 広美",
   },
@@ -212,27 +212,27 @@ const imageData = [
 ];
 
 const bios = {
-  "../img/sasagawa2.webp": `
-    <table class="manager-table table table-bordered my-5">
-      <tr><td class="col-2">会社名</td><td>国際貿易YMG株式会社</td></tr>
+  "./img/sasagawa3.webp": `
+    <table class="manager-table table table-bordered mt-5">
+      <tr><td class="col-3 col-lg-2">会社名</td><td>国際貿易YMG株式会社</td></tr>
       <tr><td>役職</td><td>代表取締役</td></tr>
       <tr><td>業種</td><td>貿易業、貿易商社、外国人材紹介、オーダースーツFC本部</td></tr>
       <tr><td>会社PR</td><td>貿易輸出入、外国人材紹介、日本語学校運営、オーダースーツ業、やってますのでご相談下さい</td></tr>
     </table>
     <div class="img-container row">
-      <a href="#desc" class="col-3"><img class="each-img4 col-12" src="../img/sasagawa2.webp" alt="笹川 政吉"></a>
-      <a href="#desc" class="col-3"><img class="each-img5 col-12" src="../img/sasagawa3.webp" alt="笹川 政吉"></a>
+      <a href="#desc" class="col-3"><img class="each-img4 col-12" src="../img/sasagawa3.webp" alt="笹川 政吉"></a>
+      <a href="#desc" class="col-3"><img class="each-img5 col-12" src="../img/sasagawa2.webp" alt="笹川 政吉"></a>
     </div>`,
-  "../img/nishikawa1.webp": `
-        <table class="manager-table table table-bordered my-5">
-          <tr><td class="col-2">会社名</td><td>東京海上あんしんエージェンシー</td></tr>
+  "./img/nishikawa1.webp": `
+        <table class="manager-table table table-bordered mt-5">
+          <tr><td class="col-3 col-lg-2">会社名</td><td>東京海上あんしんエージェンシー</td></tr>
           <tr><td>役職</td><td>代表</td></tr>
           <tr><td>業種</td><td>生保・損保代理店</td></tr>
           <tr><td>会社PR</td><td>皆様の不安をあんしんに変える為に、日々活動しております。よろしくお願い致します。</td></tr>
         </table>`,
-  "../img/tokuda1.webp": `
-    <table class="manager-table table table-bordered my-5">
-      <tr><td class="col-2">会社名</td><td>トータルビューティーサロン出逢い</td></tr>
+  "./img/tokuda1.webp": `
+    <table class="manager-table table table-bordered mt-5">
+      <tr><td class="col-3 col-lg-2">会社名</td><td>トータルビューティーサロン出逢い</td></tr>
       <tr><td>役職</td><td>代表</td></tr>
       <tr><td>業種</td><td>美容・健康</td></tr>
       <tr><td>会社PR</td><td>身体の内側から、外側からも心身経済共に豊かにする美容法、健康法、経営法を伝授！魔法のエステティシャン！</td></tr>
@@ -241,19 +241,19 @@ const bios = {
 };
 
 const representatives = {
-  "../img/sasagawa2.webp": {
+  "./img/sasagawa3.webp": {
     repTitle: "代表世話人",
     repName: "笹川 政吉",
   },
-  "../img/sasagawa3.webp": {
-    repName: "代表世話人",
-    repTitle: "笹川 政吉",
+  "./img/sasagawa2.webp": {
+    repTitle: "代表世話人",
+    repName: "笹川 政吉",
   },
-  "../img/nishikawa1.webp": {
-    repTitle: "世話人",
+  "./img/nishikawa1.webp": {
+    repTitle: "副代表世話人",
     repName: "西川 毅",
   },
-  "../img/tokuda1.webp": {
+  "./img/tokuda1.webp": {
     repTitle: "世話人",
     repName: "徳田 広美",
   },
